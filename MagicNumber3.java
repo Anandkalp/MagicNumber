44dd10489b27667020c06257b4c20c13de0a6a69
Anandkalp <anandk.neo@gmail.com>
2022-06-07 18:29:22 +0530

    added

---
+++ MagicNumber3.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+public class MagicNumber3
+{
+public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a No");
+		int num=sc.nextInt();
+		int sum=0;
+		int copy=num;
+
+		while(num>9)
+		{
+			while(num>0)
+			{
+				int mod=num%10;
+				sum=sum+mod;
+				num=num/10;
+			}
+				num=sum;
+				sum=0;
+		}
+			if(num==1)
+			{
+				System.out.println("Magic no : "+copy);
+			}
+			else 
+			{
+				System.out.println("Not a magic no : "+copy);
+		    }
+	}
+}
